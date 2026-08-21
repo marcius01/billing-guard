93f0cf4a688c7ae09849a0e256b26923b964d6fe
marcello <marcello.buoncristiano@svelto.tech>
2026-08-21 13:05:34 +0200

    [BE-DevOps] Fix CI: missing JUnit test

--- BE/bguard/app/src/test/java/tech/skullprogrammer/bguard/app/AppApplicationTests.java
+++ BE/bguard/app/src/test/java/tech/skullprogrammer/bguard/app/AppApplicationTests.java
@@ -1,13 +1,13 @@
 package tech.skullprogrammer.bguard.app;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class AppApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
