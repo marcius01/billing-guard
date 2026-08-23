670735d4baa9a8139346881192abbe2af9e0656d
marcello <marcello.buoncristiano@svelto.tech>
2026-08-23 16:43:39 +0200

    [BE] Add Kafka dependencies and first event

--- BE/bguard/api/build.gradle.kts
+++ BE/bguard/api/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation(libs.bean.validation)
     implementation(libs.open.csv)
     implementation(libs.spring.security)
+    implementation(libs.spring.boot.starter.kafka)
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
---
+++ BE/bguard/api/src/main/java/tech/skullprogrammer/bguard/api/kafka/event/ImportJobCreatedEvent.java
@@ -0,0 +1,4 @@
+package tech.skullprogrammer.bguard.api.kafka.event;
+
+public record ImportJobCreatedEvent(Long importJobId) {
+}
